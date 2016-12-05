57e7469db53cc37ec75473b358386d59af5df381
Stephen Steinkirch <ssteinkirch@dlsstudios.com>
2016-12-05 12:12:53 -0600

    Created Sensor Abstract base class and moved sensor classes to module

--- basewatcher.py
+++ basewatcher.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python
 
-import PCF8591 as ADC
-import RPi.GPIO as GPIO
 import time
 import math
-import os
-import glob
-import time
 import sys
 import sqlite3
 from twilio.rest import TwilioRestClient
-from collections import deque
 from ConfigParser import SafeConfigParser
 import smtplib
 from smtplib import SMTPException
 
-
-ADC.setup(0x48)
-GPIO.setmode(GPIO.BCM)
+import sensor
 
 conn = None
 count = 0
@@ -31,115 +23,7 @@ Twillio_AUTH_TOKEN = None
 email_from = None
 email_to = None
 smtp_url = None
-
-
-def read_temp_raw():	
-	base_dir = '/sys/bus/w1/devices/'
-	device_folder = glob.glob(base_dir + '28*')[0]
-	device_file = device_folder + '/w1_slave'
-	
-	f = open(device_file, 'r')
-	lines = f.readlines()
-	f.close()
-	return lines
-
-def read_temp():
-	lines = read_temp_raw()
-	while lines[0].strip()[-3:] != 'YES':
-		time.sleep(0.2)
-		lines = read_temp_raw()
-	equals_pos = lines[1].find('t=')
-	if equals_pos != -1:
-		temp_string = lines[1][equals_pos+2:]
-		temp_c = float(temp_string) / 1000.0	
-		temp_f = temp_c * 9.0 / 5.0 + 32.0
-		return {'c':temp_c, 'f':temp_f}
-
-
-def temp_reader():
-	def get_temp():
-		return read_temp()['f']	
-	return get_temp
-
-def log_entry(sensor_name,analog_data,binary_data):	
-		conn = sqlite3.connect('sensor_data.db')
-		dbcur = conn.cursor()
-		
-		try:
-			dbcur.execute('INSERT INTO sensor_logs(data_time, sensor_name, analog_data, gpio_data) VALUES(?,?,?,?)', (time.time(), sensor_name, analog_data, binary_data))
-			conn.commit()
-		except sqlite3.Error as er:
-			print 'er:', er.message	
-		conn.close()
-
-
-#Class to handle sensors on the PCF8591 ADDA.  Or anything that just uses one GPIO
-class Sensors:
-	GPIO_Pin = 0
-	AD_pin = 0	
-	sensor_que = None
-	que_size = 10
-	sensor_name = None
-	sensor_status = None
-	sensor_change_threshold = None
-	alert_count = None
-	GPIO_alert = 0
-	alt_function = None
-	
-	def __init__(self, sensor_name_in, GPIO_Pin_in, AD_pin_in, threshold_in, GPIO_Alert_in):
-		self.sensor_name = sensor_name_in
-		self.GPIO_Pin = GPIO_Pin_in
-		self.AD_pin = AD_pin_in		
-		self.sensor_change_threshold = threshold_in
-		GPIO.setup(GPIO_Pin_in, GPIO.IN)
-		self.sensor_que = deque()
-		self.alert_count = 0;
-		self.GPIO_alert = GPIO_Alert_in
-					
-	def logger(self):
-		log_entry(self.sensor_name, self.get_analog_data(), self.check_binary_alert())
-		
-	def avg(self):
-		total=0		
-		if len(self.sensor_que) < self.que_size:
-			return None		
-		for i in self.sensor_que:
-				total+=i	
-									
-		return total/self.que_size
-				
-	def add(self,input_data):
-		self.sensor_que.append(input_data)
-		if len(self.sensor_que) < self.que_size + 1: return
-		self.sensor_que.popleft()
-		
-	def get_analog_data(self):
-		return ADC.read(self.AD_pin)	
-		
-	"""
-	Check to see if there is a sudden change in the analog data.
-	Percent difference threshold is set when object is instantiated
-	i.e, There is a 10% difference between the current reading and the last ten readings. 
-	"""	
-	def	check_analog_alert(self):
-		before = self.avg()
-		sensor_data = self.get_analog_data()		
-		self.add(sensor_data)
-		if before is None or sensor_data is None : return False			
-		percent_diff = ((float(sensor_data)/float(before)) * 100) - 100
-		percent_diff = abs(percent_diff)
-		print 'Percent Diff ' + str(percent_diff)
-		if percent_diff >= self.sensor_change_threshold: return True		
-		return False
-		
-	def check_binary_alert(self):
-		if(GPIO.input(self.GPIO_Pin) == self.GPIO_alert):
-			return True
-		return False
-	
-	def check_alt(self):
-		return self.alt_function
-									
+						
 
 def setup():		
 	global SMS_from
@@ -149,24 +33,6 @@ def setup():
 	global email_from
 	global email_to
 	global smtp_url
-	
-		
-	os.system('modprobe w1-gpio')
-	os.system('modprobe w1-therm')
-
-	conn = sqlite3.connect('sensor_data.db')
-
-	dbcur = conn.cursor()
-	check_table = "SELECT name FROM sqlite_master WHERE type='table' AND name='sensor_logs'"
-	create_table = "CREATE TABLE sensor_logs (data_time REAL, sensor_name VARCHAR(255), analog_data REAL, gpio_data BOOLEAN)"
-	if not dbcur.execute(check_table).fetchone():
-		print "Creating sensor_logs table"
-		dbcur.execute(create_table)
-		if not dbcur.execute(check_table).fetchone():
-			print "Unable to create sensor_log table"
-			exit()
-	
-	conn.close()
 	
 	config = SafeConfigParser()
 	config.read('config.ini')
@@ -216,6 +82,7 @@ def smsalert(msg, data, force=False):
 	print 'SMS sent ' + msg
 	print ''
 
+#todo:  This has never been tested
 def emailalert(msg, data, force=False):		
 	global count
 	global msg_time
@@ -240,11 +107,12 @@ def emailalert(msg, data, force=False):
 		print ''
 		
 
-sensor_gas = Sensors('Gas',19,0,10,0)
-sensor_water = Sensors('Water', 17,None,1,0)
-sensor_flame = Sensors('Flame', 16,2,10,0)
-sensor_smoke = Sensors('Smoke', 20,1,10,0)
-sensor_co = Sensors('CO', 18,3,15,0)
+sensor_gas = sensor.sensor('Gas',19,0,10,0)
+sensor_water = sensor.water('Water', 17,None,None,0)
+sensor_flame = sensor.sensor('Flame', 16,2,10,0)
+sensor_smoke = sensor.sensor('Smoke', 20,1,10,0)
+sensor_co = sensor.sensor('CO', 18,3,15,0)
+sensor_temp = sensor.temp('Temp', None,None,20,0)
 
 
 def loop():
@@ -279,12 +147,12 @@ def loop():
 				smsalert(sensor_smoke.sensor_name + ' Alert!!!!', str(sensor_smoke.get_analog_data()))
 			print "Smoke: " + str(sensor_smoke.sensor_que)
 			
-			#the Temp sensor is unique and use the 1 wire protocol on GPIO 4
-			temp_high = 100
-			temp_low = 45
-			tempature = read_temp()['f']
-			if tempature > temp_high or tempature < temp_low:		
-				print "Temp!: " + str(tempature)
+			if sensor_temp.check_analog_alert(): 
+				print "\r" + sensor_temp.sensor_name + ' Alert!!!!' + "\r"			
+				smsalert(sensor_temp.sensor_name + ' Alert!!!!', str(sensor_temp.get_analog_data()))
+			print "Temp: " + str(sensor_temp.sensor_que)
+			
+			
 			
 		#once every hour	
 		if count % (60 * 60) == 0 and count > 0 :
@@ -293,7 +161,8 @@ def loop():
 			sensor_flame.logger()
 			sensor_co.logger()			
 			sensor_smoke.logger()
-			log_entry("Temp",tempature,None)
+			sensor_water.logger()
+			sensor_temp.logger()
 			
 		#once every Minute	
 		#if (count % 60 == 0 and count > 0) :
@@ -318,18 +187,12 @@ def loop():
 			smsalert(sensor_smoke.sensor_name + ' Alert!!!!', 'True')		
 									
 		if sensor_water.check_binary_alert():
-			print "Maybe Water"
+			print "\r" + sensor_water.sensor_name + 'Binary  Alert!!!!' + "\r"			
+			smsalert(sensor_water.sensor_name + ' Alert!!!!', 'True')	
 			
-			water_count += 1      
-			#Check ten times to make sure the signal is constant and correct 
-			if not sensor_water.check_binary_alert():
-				water_count = 0
-				
-			if water_count > 9:		
-				smsalert("Water!",'')
-				print sensor_water.sensor_name + ': ' + str(sensor_water.check_binary_alert())
-				water_count = 0
-		
+		if sensor_temp.check_binary_alert():
+			print "\r" + sensor_temp.sensor_name + 'Binary  Alert!!!!' + "\r"			
+			smsalert(sensor_temp.sensor_name + ' Alert!!!!', 'True')		
 		
 		count += 1
 				
